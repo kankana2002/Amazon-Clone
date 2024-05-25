cff080c5426c2cadcf7be34d3d7b0b72e05923d1
kankana2002 <mamonpandit2002@gmail.com>
2024-05-25 21:34:03 +0530

    first commit

---
+++ src/components/header/Navbaar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./navbaar.css";
+
+const Navbaar = () => {
+    return (
+        <header>
+            <nav>
+                <div className="left">
+                    <div className='navlogo'>
+                        <img src="https://cdn.iconscout.com/icon/free/png-512/free-amazon-credit-debit-card-bank-transaction-32279.png?f=webp&w=256" alt="" />
+
+                    </div>
+                    <div className="nav_searchbar">
+                        <input type="text" name="" id=""/>
+                        <div className='search_icon'>
+                            
+
+                        </div>
+
+                    </div>
+
+
+                </div>
+                <div className="right">
+
+                </div>
+            </nav>
+        </header>
+
+    )
+}
+
+export default Navbaar
