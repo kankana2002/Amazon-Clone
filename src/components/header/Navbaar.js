ed214582e9fbefa52fd3ca54904c47d8a2f004c1
kankana2002 <mamonpandit2002@gmail.com>
2024-05-25 22:02:02 +0530

    first commit

--- src/components/header/Navbaar.js
+++ src/components/header/Navbaar.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import "./navbaar.css";
+import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Avatar from '@mui/material/Avatar';
 
 const Navbaar = () => {
     return (
@@ -10,10 +14,10 @@ const Navbaar = () => {
                         <img src="https://cdn.iconscout.com/icon/free/png-512/free-amazon-credit-debit-card-bank-transaction-32279.png?f=webp&w=256" alt="" />
 
                     </div>
-                    <div className="nav_searchbar">
-                        <input type="text" name="" id=""/>
+                    <div className="nav_searchbaar">
+                        <input type="text" name="" id="" />
                         <div className='search_icon'>
-                            
+                            <SearchIcon id="search" />
 
                         </div>
 
@@ -22,6 +26,20 @@ const Navbaar = () => {
 
                 </div>
                 <div className="right">
+                    <div className='nav_btn'>
+                        <a href='' >Signin</a>
+                    </div>
+                    <div className='cart_btn'>
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartIcon id="icon"/>
+                            
+
+                        </Badge>
+                        <p>Cart</p>
+
+                    </div>
+                    <Avatar className='avtar'/>
+
 
                 </div>
             </nav>
