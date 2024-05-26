0b9916b925da3729d96cae73336089426cfa3854
kankana2002 <mamonpandit2002@gmail.com>
2024-05-26 16:29:07 +0530

    new

---
+++ src/components/home/productdata.js
@@ -0,0 +1,87 @@
+export const products = [
+    {
+        id: 'product1',
+        url: 'https://m.media-amazon.com/images/I/61cxcFu7wDL._SX679_.jpg',
+        detailUrl: 'https://www.amazon.in/Philips-HD9373-00-Kettle-thicker/dp/B0C7CQ6WXP/ref=sxin_15_pa_sp_search_thematic_sspa?content-id=amzn1.sym.ee635213-fee1-408a-be4a-3523feeb9a42%3Aamzn1.sym.ee635213-fee1-408a-be4a-3523feeb9a42&crid=3G8QFYVOP8JP9&cv_ct_cx=electric%2Bkettle&dib=eyJ2IjoiMSJ9.OhUSQAUoFTdpHw9RBKAIui9u4_h9IdH8a24qJMvLfWFhCzP1jXcbMuedP1SaxO244kWeMe6vxLtlOrPQJExJ8g.jlm6yvUM0S6Yj4uX2ym8Tps9QoiCsBuYmlVIM4A9w7Q&dib_tag=se&keywords=electric%2Bkettle&pd_rd_i=B0C7CQ6WXP&pd_rd_r=2eaf9eb7-811e-4ad3-83e3-4723ddbf4258&pd_rd_w=iXEea&pd_rd_wg=EYfmt&pf_rd_p=ee635213-fee1-408a-be4a-3523feeb9a42&pf_rd_r=N6XBWZA176D2DQRE8QQN&qid=1716720789&s=kitchen&sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D&sprefix=elec%2Ckitchen%2C201&sr=1-3-ced4eeeb-b190-41d6-902a-1ecb3fb8b7c4-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9zZWFyY2hfdGhlbWF0aWM&th=1',
+        title: {
+            shortTitle: 'Home & Kitchen',
+            longTitle: 'Philips HD9373/00 1.5 L Kettle with 25% thicker body for longer life, triple safe auto cut off'
+        },
+        price: {
+            mrp: 2295,
+            cost: 999,
+            discount: '56%'
+        },
+        description: 'This electric kettle from Pigeon will soon become a travelers best friend, a hostelite saviour and an answer to all the midnight cravings. With this handy appliance, you can boil water and use it to make instant noodles, packet soup, coffee and green tea.',
+        discount: 'Extra 10% Off',
+        tagline: 'Deal of the day'
+    },
+    {
+        id: 'product2',
+        url: 'https://rukminim1.flixcart.com/image/416/416/kl6wx3k0/sandwich-maker/8/r/d/sandwich-01-flipkart-smartbuy-original-imagydds4zthxt8z.jpeg?q=70',
+        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kl6wx3k0/sandwich-maker/8/r/d/sandwich-01-flipkart-smartbuy-original-imagydds4zthxt8z.jpeg?q=70',
+        title: {
+            shortTitle: 'Sandwich Makers',
+            longTitle: 'Flipkart SmartBuy Sandwich 01 Grill  (Black)'
+        },
+        price: {
+            mrp: 1499,
+            cost: 899,
+            discount: '40%'
+        },
+        description: 'This non-stick sandwich toaster .easy to use and very handy. Directly hold over flame to make tasty toasts and toasted sandwiches. Specially designed by keeping your needs in mind, the sandwich maker makes whatever youre doing simpler, smarter and better',
+        discount: 'From 99+5% Off',
+        tagline: 'Pestige, Nova & more'
+    },
+    {
+        id: 'product3',
+        url: 'https://rukminim1.flixcart.com/image/150/150/kohigsw0/resistance-tube/c/s/e/new-adjustable-single-resistance-tube-multicolor-na-ajro-deal-original-imag2xg88mhmwxz5.jpeg?q=70',
+        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kohigsw0/resistance-tube/c/s/e/new-adjustable-single-resistance-tube-multicolor-na-ajro-deal-original-imag2xg88mhmwxz5.jpeg?q=70',
+        title: {
+            shortTitle: 'Fitness Gear',
+            longTitle: 'AJRO DEAL New Adjustable Single Resistance Tube (Multicolor) Resistance Tube  (Multicolor)'
+        },
+        price: {
+            mrp: 499,
+            cost: 166,
+            discount: '66%'
+        },
+        description: 'This unique product can tone your back muscles, reduce belly fat, improve blood circulation and also improves your body posture. It increases the stamina, energy and vitality of the body. The elastic resistance of the rubber training rope can be used to train and exercise in whichever way you want, according to your physical needs.',
+        discount: 'Upto 70% Off',
+        tagline: 'Deal of the Day'
+    },
+    {
+        id: 'product4',
+        url: 'https://rukminim1.flixcart.com/image/300/300/kll7bm80/smartwatch/c/1/n/43-mo-sw-sense-500-android-ios-molife-original-imagyzyycnpujyjh.jpeg?q=70',
+        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kll7bm80/smartwatch/c/1/n/43-mo-sw-sense-500-android-ios-molife-original-imagyzyycnpujyjh.jpeg?q=70',
+        title: {
+            shortTitle: 'Smart Watches',
+            longTitle: 'Molife Sense 500 Smartwatch  (Black Strap, Freesize)',
+        },
+        price: {
+            mrp: 6999,
+            cost: 4049,
+            discount: '42%'
+        },
+        description: 'The Molife Sense 500, a brilliant smartwatch with a beautiful large display. Say hello to the infinity 1.7-inch display with 2.5D curved edges. Thanks to seamless Bluetooth 5.0 connectivity, you wont have to keep waiting. Bring a change to your outfit every day with changeable straps. A splash of color every day keeps the boredom away.',
+        discount: 'Grab Now',
+        tagline: 'Best Seller'
+    },
+    {
+        id: 'product5',
+        url: 'https://rukminim1.flixcart.com/image/416/416/k3uhhu80/hair-dryer/n/m/t/nova-2800-w-professional-nhp-8220-original-imafmvwfhmzsxdrw.jpeg?q=70',
+        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/k3uhhu80/hair-dryer/n/m/t/nova-2800-w-professional-nhp-8220-original-imafmvwfhmzsxdrw.jpeg?q=70',
+        title: {
+            shortTitle: 'Trimmers, Dryers & more',
+            longTitle: 'Nova Professional NHP 8220 Hair Dryer  (1800 W, Multicolor)'
+        },
+        price: {
+            mrp: 1899,
+            cost: 1124,
+            discount: '40%'
+        },
+        description: '',
+        discount: 'From ₹499',
+        tagline: 'Kubra, Nova & more'
+    }
+]
