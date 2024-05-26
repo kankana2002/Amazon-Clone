3db6cd15613a89dbe46a640fd4a61c964d622a51
kankana2002 <mamonpandit2002@gmail.com>
2024-05-26 23:16:57 +0530

    commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import "./footer.css";
+import Button from '@mui/material/Button';
+
 
 
 
@@ -7,65 +10,74 @@ import React from 'react'
 const Footer = () => {
     const year = new Date().getFullYear();
     console.log(year);
-  return (
-    <footer>
-        <div className='footer_container'>
-            <div className='footr_details_one'>
-                <h3>Get to Know Us</h3>
-                <p>About Us</p>
-                <p>Careers</p>
-                <p>Press Releases</p>
-                <p>Amazon Science</p>
+    return (
+        <footer>
+            <div className='footer_container'>
+                <div className='footr_details_one'>
+                    <h3>Get to Know Us</h3>
+                    <p>About Us</p>
+                    <p>Careers</p>
+                    <p>Press Releases</p>
+                    <p>Amazon Science</p>
+
+                </div>
+                <div className='footr_details_one'>
+                    <h3>Connect with Us</h3>
+                    <p>Facebook</p>
+                    <p>Twitter</p>
+                    <p>Instagram</p>
+
+
+                </div>
+                <div className='footr_details_one forres'>
+                    <h3>Let Us Help You</h3>
+                    <p>COVID-19 and Amazon</p>
+                    <p>Your Account</p>
+                    <p>Returns Centre</p>
+                    <p>100% Purchase Protection</p>
+                    <p>Amazon App Download</p>
+                    <p>Help</p>
+
+                </div>
+                <div className='footr_details_one forres'>
+                    <h3>Make Money with Us</h3>
+                    <p>Sell under Amazon Accelerator</p>
+                    <p>Protect and Build Your Brand</p>
+                    <p>Amazon Global Selling</p>
+                    <p>Become an Affiliate</p>
+                    <p>Fulfilment by Amazon</p>
+                    <p>Advertise Your Products</p>
+                    <p>Amazon Pay on Merchants</p>
+
+
+                </div>
 
             </div>
-            <div className='footr_details_one'>
-                <h3>Connect with Us</h3>
-                <p>Facebook</p>
-                <p>Twitter</p>
-                <p>Instagram</p>
+
+
+            <div className='lastdetails'>
+                
+                <img src='https://raw.githubusercontent.com/harsh17112000/E-commerceapp/main/client/public/amazon_PNG25.png' />
+                
+                <Button variant="outlined" className='btn'>India</Button>
+                
                 
 
-            </div>
-            <div className='footr_details_one forres'>
-                <h3>Let Us Help You</h3>
-                <p>COVID-19 and Amazon</p>
-                <p>Your Account</p>
-                <p>Returns Centre</p>
-                <p>100% Purchase Protection</p>
-                <p>Amazon App Download</p>
-                <p>Help</p>
 
-            </div>
-            <div className='footr_details_one forres'>
-                <h3>Make Money with Us</h3>
-                <p>Sell under Amazon Accelerator</p>
-                <p>Protect and Build Your Brand</p>
-                <p>Amazon Global Selling</p>
-                <p>Become an Affiliate</p>
-                <p>Fulfilment by Amazon</p>
-                <p>Advertise Your Products</p>
-                <p>Amazon Pay on Merchants</p>
+
+                <p>Conditions of Use & Sale
+                    Privacy Notice
+                    Interest-Based Ads</p>
+                <p>© 1996-{year}, Amazon.com, Inc. or its affiliates</p>
+
+
 
 
             </div>
+           
 
-        </div>
-        <div className='lastdetails'>
-            <img src='https://cdn.iconscout.com/icon/free/png-512/free-amazon-1869030-1583154.png?f=webp&w=256' />
-
-            
-            <p>Conditions of Use & Sale
-Privacy Notice
-Interest-Based Ads</p>
-<p>© 1996-{year}, Amazon.com, Inc. or its affiliates</p>
-
-            
-
-
-        </div>
-
-    </footer>
-  )
+        </footer>
+    )
 }
 
 export default Footer
